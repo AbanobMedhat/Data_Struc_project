6afc9b4399824f9aec10cf9c223d8d223c3bf6bc
hebamosafa <36491097+hebamosafa@users.noreply.github.com>
2019-06-27 23:58:09 +0200

    Add files via upload
    Degree Algorithm with testing code

---
+++ Data_Struc Project/Data_Struc Project/Degree_Algorithm/Degree_Algorithm/Degree_Algorithm.cpp
@@ -0,0 +1,166 @@
+// Degree_Algorithm.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+
+#include<vector>
+using namespace std;
+class edge {
+private:
+	int src;
+	int des;
+	int weight;
+public:
+	edge()
+	{}
+	edge(int in_src, int in_des, int in_weight)
+	{
+		src = in_src;
+		des = in_des;
+		weight = in_weight;
+	}
+	int get_src()
+	{
+		return src;
+	}
+	void set_src(int source)
+	{
+		src = source;
+	}
+	int get_des()
+	{
+		return des;
+	}
+	void set_des(int destination)
+	{
+		des = destination;
+	}
+	int get_weight()
+	{
+		return weight;
+	}
+	void set_weight(int wght)
+	{
+		weight = wght;
+	}
+};
+class graph {
+private:
+	edge grph_edges[100];
+	int cnt_edges;
+	vector<int>nodes;
+	int cnt_nodes;
+public:
+	graph()
+	{
+		cnt_edges = 0;
+	}
+	void set_nodes(int no_nodes)
+	{
+		nodes.resize(no_nodes);
+		cnt_nodes = no_nodes;
+	}
+	int get_no_nodes()
+	{
+		return cnt_nodes;
+	}
+	int get_no_edges()
+	{
+		return cnt_edges;
+	}
+
+	void set_edge(edge edg)
+	{
+		grph_edges[cnt_edges] = edg;
+		cnt_edges++;
+	}
+	edge get_edge(int index)
+	{
+		return grph_edges[index];
+	}
+	edge top_edge()
+	{
+		return grph_edges[0];
+	}
+	edge last_edge()
+	{
+		return grph_edges[cnt_edges - 1];
+	}
+	void take_input(int no_nodes, int no_edges)
+	{
+		int src, des, weight;
+		for (int i = 0;i < no_edges;i++)
+		{
+			cin >> src >> des >> weight;
+			edge in_edge(src, des, weight);
+			this->set_edge(in_edge);
+			this->set_nodes(no_nodes);
+		}
+	}
+	//shortest path function you are free to change the return type of the function
+	void shortest_path(int src, vector<vector<int>>table)
+	{
+		//write your code here 
+	}
+};
+class base_algorithem {
+public:
+	virtual void process() = 0;
+};
+class degree_algorithem :public base_algorithem {
+private:
+	graph sys;
+	int* Degree_cen;
+	edge graph_edges;
+	//write degree algorithem here
+public:
+	degree_algorithem(graph sys) {
+		this->sys = sys;
+	    Degree_cen = new int[sys.get_no_nodes()]{ 0 };
+	}
+	void process()
+	{
+		for (int i = 0;i<sys.get_no_edges() ;i++)
+		{
+			graph_edges =sys.get_edge(i);
+			Degree_cen[graph_edges.get_src()]++;
+			Degree_cen[graph_edges.get_des()]++;
+		}
+
+	}
+	void test() {
+		for (int i = 0;i < sys.get_no_nodes();i++)
+		{
+			cout << Degree_cen[i] << "   ";
+		}
+	}
+};
+class centralized_algorithem :public base_algorithem {
+public:
+	void process()
+	{
+		//write your code here
+	}
+};
+class betweness_algorithem :public base_algorithem {
+public:
+	void process()
+	{
+		//write your code here 
+	}
+};
+int main()
+{
+	int no_nodes, no_edges;
+	cin >> no_nodes >> no_edges;
+	graph project_sys;
+	project_sys.take_input(no_nodes, no_edges);
+	/////////test degree algorithm
+	degree_algorithem Degree_Test(project_sys);
+	Degree_Test.process();
+	Degree_Test.test();
+	/////////////////////////
+	system("pause");
+	return 0;
+}
